f4ba6db5e8f9c64065aea57d34b46767026bfe83
micasRod <rodriguez.mcsol@gmail.com>
2025-04-18 00:18:19 -0300

    Add exercises of basic php sentences

---
+++ TestPHP/testPHP.php
@@ -0,0 +1,12 @@
+<?php
+// Mi primer script PHP - Hola Mundo
+echo "<h1>¡Hola Mundo!</h1>";
+echo "<p>Esta es mi primera página con PHP.</p>";
+
+// También podemos mostrar la fecha y hora actual
+echo "<p>Fecha y hora actual: " . date("d/m/Y H:i:s") . "</p>";
+
+// Podemos usar variables
+$nombre = "Usuario";
+echo "<p>¡Bienvenido, $nombre!</p>";
+?>
